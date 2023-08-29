583acf93c5d3f57579efd4c845427543619b6704
nishamokashe <nishamokase18@gmail.com>
2023-08-29 20:28:09 +0530

    Addition.java
    Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+ class Addition
+{
+ public static void main(String[] args)
+  {
+    int num1 = 18;
+    int num2 = 42;
+
+    int result = num1+num2;
+    System.out.println("Addition is"+result);   
+  }
+}
