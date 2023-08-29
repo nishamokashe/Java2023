457f9856ee24ca7cc3937a53a44f776aff5729eb
nishamokashe <nishamokase18@gmail.com>
2023-08-29 20:29:11 +0530

    Division.java
    Division.java

---
+++ Division.java
@@ -0,0 +1,10 @@
+public class Division 
+{
+    public static void main(String[] args)
+    {
+       double a=40, b=15, result;
+       result= a/b;
+       System.out.println("Division is"+result);    
+    }
+ 
+}
