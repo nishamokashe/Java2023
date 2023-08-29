817da8f40412f63f5410a59a6c6a276940945865
nishamokashe <nishamokase18@gmail.com>
2023-08-29 20:29:27 +0530

    Multiplication.java
    Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+class Multiplication
+{
+    public static void main(String[] args)
+    {
+       double a=87, b=23, result;
+       result= a*b;
+       System.out.println("Multiplication is"+result);    
+    }
+}
