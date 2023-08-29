0ab5983055989d4dca1f56111e8283ec73ad18d1
nishamokashe <nishamokase18@gmail.com>
2023-08-29 20:28:42 +0530

    Cylender.java
    Cylender.java

---
+++ Cylender.java
@@ -0,0 +1,12 @@
+//WAP to calaculate area of cylender
+class Cylender 
+{
+public static void main(String[] args) 
+ {
+    {
+    double radius=3.2, height=2.5 , area;
+    area=(2*(22/7)*radius*height);
+	System.out.println("\n\n Area of Cylnder is"+area);
+   }
+ }
+}
