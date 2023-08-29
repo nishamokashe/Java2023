e5ac63b3218115b156635977269c688115dcbcb7
nishamokashe <nishamokase18@gmail.com>
2023-08-29 20:29:41 +0530

    Sub.java
    Sub.java

---
+++ Sub.java
@@ -0,0 +1,11 @@
+ class Sub
+{
+ public static void main(String[] args)
+  {
+    int num1 = 56;
+    int num2 = 32;
+
+    int result = num1-num2;
+    System.out.println("Sub is"+result);   
+  }
+}
