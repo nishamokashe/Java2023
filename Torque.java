9860b9da865dd1ec2c0274477bc980429235c85a
nishamokashe <nishamokase18@gmail.com>
2023-08-29 20:29:57 +0530

    Torque.java
    Torque.java

---
+++ Torque.java
@@ -0,0 +1,12 @@
+
+//WAP to calculate the area of troque
+
+class Torque
+{
+  public static void main(String[] args)
+  {
+   double m1= 17, m2= 21,area;
+   area =(m1+m2/m1*m2);
+   System.out.println("\n\n Torque is:"+area);
+  }
+}
