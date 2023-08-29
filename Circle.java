afdbbede47a745cbecda5c52c219bba847e79af3
nishamokashe <nishamokase18@gmail.com>
2023-08-29 20:28:28 +0530

    Circle.java
    Circle.java

---
+++ Circle.java
@@ -0,0 +1,11 @@
+//Area.java
+class Circle
+{
+  public static void main(String args[])
+  
+  {
+    double pi =(22/7), radius=3.2, result;
+	result=(pi*radius*radius);
+	System.out.println(" \n\n Area of circle is"+result);
+  }
+}
