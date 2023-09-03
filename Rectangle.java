3ddca32b2a7b3a544207747ad8be047f213e093b
nishamokashe <nishamokase18@gmail.com>
2023-09-03 23:16:34 +0530

    Rectangle.java
    Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+
+//WAP to calculate the area of rectangle 
+class Rectangle
+{
+     public static void main(String[] args) 
+    {
+        // Area of rectangle (Area= length*width)
+        double l=22, w=15, area;
+        area= (l*w);
+        System.out.println("\n\n Area of rectangle is"+area);
+    }
+}
