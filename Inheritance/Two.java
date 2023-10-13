66a9e32af0996e234f92f7eb5c40b1bb84c0de09
nishamokashe <nishamokase18@gmail.com>
2023-10-13 21:06:15 +0530

    Two.java
    Two.java

---
+++ Inheritance/Two.java
@@ -0,0 +1,23 @@
+/* Write a Java program to create a class called Animal with a method called makeSound(). Create a subclass 
+called Cat that overrides the makeSound() method to bark.*/
+
+ class Animal
+ {
+    void makeSound()
+    {
+        System.out.println("The Cat makes a sound.");
+    }
+ }
+ class Cat extends Animal
+ {
+    void makeSound()
+    {
+        System.out.println("meow");
+    }
+ 
+    public static void main(String[] args) 
+    {
+       Cat c = new Cat();
+       c.makeSound();
+    }
+ }
