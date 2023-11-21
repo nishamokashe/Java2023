2df2b60f1646c17e7408522aca3e7ab84fe546a5
nishamokashe <nishamokase18@gmail.com>
2023-11-21 18:47:28 +0530

    Table.java

--- LoopingStatement/Table.java
+++ LoopingStatement/Table.java
@@ -14,7 +14,7 @@ public class Table
                 
         System.out.println("Multiplication Table of " + n);
         
-        for(int i=1; i<=10; i++)
+        for(int i=1;i<=10; i++)
         {
             System.out.println(n +" x " + i + " = " + (n*i) );
         }
