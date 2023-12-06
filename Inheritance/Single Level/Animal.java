9f496b51c51bfc74ee5b95d6b0b9c5ebea52f732
nishamokashe <nishamokase18@gmail.com>
2023-12-06 21:16:49 +0530

    Update Animal.java
    Update Animal.java

--- Inheritance/Single Level/Animal.java
+++ Inheritance/Single Level/Animal.java
@@ -13,8 +13,6 @@ class Cat extends Animal
     }
   public static void main(String[] args) 
   {
-    Animal a = new Animal();
-    a.makeSound();
     Cat c = new Cat();  
     c.makeSound();
   }  
